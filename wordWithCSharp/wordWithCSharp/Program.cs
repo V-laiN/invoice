06aca1b5524d9001f1f00ee7f254d40fad4fcbd9
V-laiN <94229211+V-laiN@users.noreply.github.com>
2024-04-18 16:31:59 +0530

    Add files via upload

---
+++ wordWithCSharp/wordWithCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+namespace word
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+            object oEndOfDoc = "\\endofdoc"; 
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+            oWord.Visible = true;
+            oDoc = oWord.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+        }
+    }
+}
